e530293375601d2ec36c4ce8fa4884cd2c753353
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-08-18 03:12:15 -0500

    model association rspec green
    presence validators add

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,8 @@
 class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :service
+
+  validates :date, presence: true
+  validates :time, presence: true
+  validates :description, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  has_many :appointments, dependent: :destroy
+
+  validates :name, presence: true
+  validates :password, presence: true
+  validates :email, presence: true
+
+  has_secure_password
 end
