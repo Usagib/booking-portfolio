db7e8297d5aa58ef0ba94d574ecd76bcffd76e6a
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-08-18 02:58:26 -0500

    db references and presence validators add to rspec

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Service, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:appointments) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:max_cost) }
+  it { should validate_presence_of(:min_cost) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:appointments).dependent(:destroy) }
+  it { should_validate_presence_of(:name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password_digest) }
 end
