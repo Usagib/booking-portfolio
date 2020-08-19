bc2279549fd1eae5b7a5f0af6516e288088525a6
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-08-18 20:05:43 -0500

    services controller update
    add exception_handler and response to concern

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,17 +1,34 @@
 class ServicesController < ApplicationController
+  before_action :set_service, only[:show, :update, :destroy]
+
   def index
+    @services = Service.all
+    render json: @services, status: :ok
   end
 
   def show
+    render json: @service, status: :ok
   end
 
   def create
+    @service = Service.new(service_params)
+    if @service.save
+      render json: @service, status :created
+    else
+      render json: @service, status :unprocessable_entity
+    end
   end
 
   def update
+    if @service.update(service_params)
+      render json: @service
+    else
+      render json: @service.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @service.destroy
   end
 
   private
