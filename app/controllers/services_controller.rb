995b0303c97063cf6c0cf757eb0e14ad52915a50
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-08-18 19:49:48 -0500

    services controller init

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,2 +1,25 @@
 class ServicesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def service_params
+    params.permit(:name, :description, :max_cost, :min_cost)
+  end
+
+  def set_service
+    @service = Service.find(params[:id])
+  end
 end
