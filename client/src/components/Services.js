fce73ce189cbf8beb88eb1b8d71fe591ae9bb42d
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-09-06 16:30:52 -0500

    new services frontend

--- client/src/components/Services.js
+++ client/src/components/Services.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { faCog, faPlayCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class Services extends React.Component {
   constructor(props) {
@@ -22,7 +24,7 @@ class Services extends React.Component {
             <div
               className="carousel-item active"
               style={{
-                background: 'url(\'3dprint.png\')',
+                background: 'url(\'support.png\')',
                 backgroundRepeat: 'no-repeat',
                 backgroundSize: 'cover',
                 backgroundPosition: 'center',
@@ -30,14 +32,18 @@ class Services extends React.Component {
             >
               <div className="carousel-caption d-none d-md-block">
                 <span>
-                  <h1 className="display-2 text-white">3d Printing</h1>
+                  <h1 className="carousel-title text-white">NEW AVAILABLE SERVICES</h1>
                 </span>
-                <button type="button" className="btn-dark btn float-center">
-                  <a href="/serviceselect"><h1 className="display-5">Require a service</h1></a>
+                <button type="button" className="btn configure-btn float-center">
+                  <a href="/serviceselect">
+                    <FontAwesomeIcon icon={faCog} />
+                      <span className="mx-2">Configure</span>
+                    <FontAwesomeIcon icon={faPlayCircle} />
+                  </a>
                 </button>
               </div>
             </div>
-            <div
+          {/*  <div
               className="carousel-item"
               style={{
                 background: 'url(\'project.png\')',
@@ -48,7 +54,7 @@ class Services extends React.Component {
             >
               <div className="carousel-caption d-none d-md-block">
                 <span>
-                  <h1 className="display-2 text-white">Project Management</h1>
+                  <h1 className="carousel-title text-white">Project Management</h1>
                 </span>
                 <button type="button" className="btn-dark btn float-center">
                   <a href="/serviceselect"><h1 className="display-5">Require a service</h1></a>
@@ -66,7 +72,7 @@ class Services extends React.Component {
             >
               <div className="carousel-caption d-none d-md-block">
                 <span>
-                  <h1 className="display-2 text-white">Technical Support</h1>
+                  <h1 className="carousel-title text-white">Technical Support</h1>
                 </span>
                 <button type="button" className="btn-dark btn float-center">
                   <a href="/serviceselect"><h1 className="display-5">Require a service</h1></a>
@@ -84,7 +90,7 @@ class Services extends React.Component {
             >
               <div className="carousel-caption d-none d-md-block">
                 <span>
-                  <h1 className="display-2 text-white">Stationery Design</h1>
+                  <h1 className="carousel-title text-white">Stationery Design</h1>
                 </span>
                 <button type="button" className="btn-dark btn float-center">
                   <a href="/serviceselect"><h1 className="display-5">Require a service</h1></a>
@@ -102,7 +108,7 @@ class Services extends React.Component {
             >
               <div className="carousel-caption d-none d-md-block">
                 <span>
-                  <h1 className="display-2 text-dark">Backend Development</h1>
+                  <h1 className="carousel-title text-dark">Backend Development</h1>
                 </span>
                 <button type="button" className="btn-dark btn float-center">
                   <a href="/serviceselect"><h1 className="display-5">Require a service</h1></a>
@@ -120,7 +126,7 @@ class Services extends React.Component {
             >
               <div className="carousel-caption d-none d-md-block">
                 <span>
-                  <h1 className="display-2 text-dark">Frontend Design</h1>
+                  <h1 className="carousel-title text-dark">Frontend Design</h1>
                 </span>
                 <button type="button" className="btn-dark btn float-center">
                   <a href="/serviceselect"><h1 className="display-5">Require a service</h1></a>
@@ -138,13 +144,13 @@ class Services extends React.Component {
             >
               <div className="carousel-caption d-none d-md-block">
                 <span>
-                  <h1 className="display-2 text-dark">Graphic Design</h1>
+                  <h1 className="carousel-title text-dark">Graphic Design</h1>
                 </span>
                 <button type="button" className="btn-dark btn float-center">
                   <a href="/serviceselect"><h1 className="display-5">Require a service</h1></a>
                 </button>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </header>
